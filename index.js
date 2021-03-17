8105442cbd3f31063fac36e1db00bfe66e0274cd
Xunxin Li <xli9400@Xunxins-MBP.lan1>
2021-03-17 14:13:36 -0400

    Finish Task 1

--- index.js
+++ index.js
@@ -45,7 +45,12 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
+function copy(arry){
+    let newArry =[];
+    for(let i=0;i<arry.length;i++){
+        newArry.push(arry[i]);
+    }
+    return newArry;
     /*your code here*/
 }    
 
